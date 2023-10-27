dd2fae8bb0781d2ff8db2e62203d9bb00155ab8d
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2023-10-27 15:57:17 +0100

    Code php file to send form to email

--- form-to-email.php
+++ form-to-email.php
@@ -1,14 +1,32 @@
 <?php
-if (!isset($_POST["submit"]))
-{
-    echo("Error: You need to submit the form");
-}
 
 // Accessing Form submission data:
 $name = $_POST["name"];
 $userEmail = $_POST["email"];
 $userAge = $_POST["age"];
-$contactReason = filter_input(INPUT_POST, "reason", FILTER_SANITIZE_STRING);
+$contactReason = $_POST["reason"];
+$developer = $_POST["dev"];
+$devArea = $_POST["area"];
 $message = $_POST["message"];
 
+// Compossing the email message:
+$to      = "[Put your email address here]";
+$subject = "Contact Form Submission";
+$message = "Hello, 
+    You have got a new submission message:
+    Name: $name 
+    Email: $userEmail
+    Age: $userAge
+    The contact reason is: $contactReason
+    $name is a $developer developer
+    Message:
+    $message .
+    ";
+$area[] = $devArea;
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+// Sending the email:
+mail($to, $subject, $message, $headers);
 ?>
